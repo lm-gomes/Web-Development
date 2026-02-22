a1bdccb8a6c6d86392f75902bde110e38b9ccfc1
lmgomes <lm.moreiragomes@gmail.com>
2026-02-21 23:26:24 -0300

    springboot backend added for login auth service

---
+++ Sites/ByteForge/byteforge/src/main/java/com/project/byteforge/ByteforgeApplication.java
@@ -0,0 +1,13 @@
+package com.project.byteforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteforgeApplication.class, args);
+	}
+
+}
